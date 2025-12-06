c267699f3e7464080acd164e3d1079dba72db13b
DanH629 <thegodninja10@gmail.com>
2025-12-06 10:51:46 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-var app = require('express')() ;
-var http = require('http').createServer(app) ;
-var io = require('socket.io')(http,{
-     cors: { origin: "*" } 
-}) ;
+const express = require("express");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+    cors: { origin: "*" }
+});
 
-io.on('connection', (socket) => {
-    console.log('a user Online  ') ;
-    socket.on('canvas-data', (data) => {
-        socket.broadcast.emit('canvas-data', data) ;
-    })
-}) 
-var server_port = process.env.YOURPORT || process.env.PORT || 5000 ;
-http.listen(server_port, () => {
-    console.log("Start on :" + server_port) ;
-}) 
+// Khi user vào kết nối
+io.on("connection", (socket) => {
+    console.log("A user connected");
+
+    // Nhận dữ liệu vẽ và gửi cho người khác trong phòng
+    socket.on("canvas-data", (data) => {
+        socket.broadcast.emit("canvas-data", data); // send to all except sender
+    });
+
+    socket.on("disconnect", () => {
+        console.log("A user disconnected");
+    });
+});
+
+// PORT do Render cấp, không được dùng 5000 cố định
+const PORT = process.env.PORT || 10000;
+
+http.listen(PORT, () => {
+    console.log("Socket server running on port: " + PORT);
+});
